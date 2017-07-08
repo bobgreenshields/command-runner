8b0dbd5600793cd39b53e8c910b71c819e8b6157
Bob Greenshields <bob@thegreenshields.com>
2017-07-08 15:14:26 +0100

    start specs for Response

---
+++ lib/command_runner.rb
@@ -0,0 +1,5 @@
+module CommandRunner
+	class Response
+		
+	end
+end
---
+++ spec/command_runner_response_spec.rb
@@ -0,0 +1,17 @@
+require "command_runner"
+
+describe CommandRunner::Response do
+	describe "#new" do
+		it "should return a a Response instance with the correct attributes" do
+			response = CommandRunner::Response.new(cmd_str: "test string",
+																						 stdout: "stdout",
+																						 stderr: "stderr",
+																						 exit_code: 0)
+			expect(response).to be_an_instance_of CommandRunner::Response
+			expect(response.cmd_str).to eql("test_str")
+			expect(response.stdout).to eql("stdout")
+			expect(response.stderr).to eql("stderr")
+			expect(response.exit_code).to eql(0)
+		end
+	end
+end
