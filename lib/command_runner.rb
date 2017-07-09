850825cc50b6422cb5a96e52fc5b3dfbf10e5fba
Bob Greenshields <bob@thegreenshields.com>
2017-07-09 09:52:49 +0100

    start response object with #initialize

--- lib/command_runner.rb
+++ lib/command_runner.rb
@@ -1,5 +1,12 @@
 module CommandRunner
 	class Response
-		
+		attr_reader :cmd_str, :stdout, :stderr, :exit_code
+
+		def initialize(cmd_str:, stdout:, stderr:, exit_code:)
+			@cmd_str = cmd_str
+			@stdout = stdout
+			@stderr = stderr
+			@exit_code = exit_code
+		end
 	end
 end
