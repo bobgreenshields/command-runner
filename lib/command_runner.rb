183300b9ae144d8c33df608482f3af7fc2477a96
Bob Greenshields <bob@thegreenshields.com>
2017-07-09 13:07:24 +0100

    finish Response object

--- lib/command_runner.rb
+++ lib/command_runner.rb
@@ -8,5 +8,17 @@ module CommandRunner
 			@stderr = stderr
 			@exit_code = exit_code
 		end
+
+		def success?
+			@exit_code == 0
+		end
+
+		def on_success
+			yield self if success?
+		end
+
+		def on_failure
+			yield self unless success?
+		end
 	end
 end
--- spec/command_runner_response_spec.rb
+++ spec/command_runner_response_spec.rb
@@ -4,9 +4,7 @@ describe CommandRunner::Response do
 	describe "#new" do
 		it "returns a Response instance with the correct attributes" do
 			response = CommandRunner::Response.new(cmd_str: "test string",
-																						 stdout: "stdout",
-																						 stderr: "stderr",
-																						 exit_code: 0)
+				stdout: "stdout", stderr: "stderr", exit_code: 0)
 			expect(response).to be_an_instance_of CommandRunner::Response
 			expect(response.cmd_str).to eql("test string")
 			expect(response.stdout).to eql("stdout")
@@ -14,4 +12,62 @@ describe CommandRunner::Response do
 			expect(response.exit_code).to eql(0)
 		end
 	end
+
+	def new_response
+		response = CommandRunner::Response.new(cmd_str: "test string",
+			stdout: "stdout", stderr: "stderr", exit_code: exit_code)
+	end
+
+	describe "#success?" do
+		context "when the exit code is zero" do
+			let(:exit_code) { 0 }
+			it "returns true" do
+				expect(new_response).to be_success
+			end
+		end
+
+		context "when the exit code is non-zero" do
+			let(:exit_code) { 1 }
+			it "returns false" do
+				expect(new_response).not_to be_success
+			end
+		end
+	end
+
+	describe "#on_success" do
+		context "when the exit code is zero" do
+			let(:exit_code) { 0 }
+			it "yields with self" do
+				response = new_response
+				expect { |b| response.on_success(&b) }.to yield_with_args(response)
+			end
+		end
+
+		context "when the exit code is non-zero" do
+			let(:exit_code) { 1 }
+			it "does not yield" do
+				response = new_response
+				expect { |b| response.on_success(&b) }.not_to yield_with_args
+			end
+		end
+	end
+
+	describe "#on_failure" do
+		context "when the exit code is zero" do
+			let(:exit_code) { 0 }
+			it "does not yield" do
+				response = new_response
+				expect { |b| response.on_failure(&b) }.not_to yield_with_args
+			end
+		end
+
+		context "when the exit code is non-zero" do
+			let(:exit_code) { 1 }
+			it "yields with self" do
+				response = new_response
+				expect { |b| response.on_failure(&b) }.to yield_with_args(response)
+			end
+		end
+	end
+
 end
